71d6929a7225580c1836440ed05ef00459d75411
worawitkaew <mon2350@hotmail.com>
2026-06-18 10:04:48 +0700

    ผ่าน FastAPI ขั้น 2

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-from fastapi import FastAPI,Request,Depends
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from routes.api import api_router
+app = FastAPI()
 
-from routes.utility.login import get_current_active_user
-from contextlib import asynccontextmanager
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup event (runs before the app starts)
-    print("App is starting")
-    yield
-    # Shutdown event (runs before the app shuts down)
-    print("App is shutting down")
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(api_router, prefix="/routes")
 
 @app.get("/")
-def root(current_user=Depends(get_current_active_user)):
-    return {"message": "Hello World","current_user":current_user}
-
-import time
-@app.get("/test")
-def root_test():
-    time.sleep(2)
-    return {"message_test": "Hello World_test"}
+def root():
+    return {
+        "message": "ร้านดาวตก"
+    }
 
 
+@app.get("/products")
+def get_products():
+
+    return [
+        {
+            "id": 1,
+            "name": "น้ำสปอนเซอร์",
+            "price": 15
+        },
+        {
+            "id": 2,
+            "name": "น้ำเปล่า",
+            "price": 10
+        },
+        {
+            "id": 3,
+            "name": "มด",
+            "price": 300
+        }
+    ]
