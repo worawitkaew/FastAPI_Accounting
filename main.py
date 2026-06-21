620844d02b6e35587cf02531d0a603476240baa1
worawitkaew <mon2350@hotmail.com>
2026-06-21 11:44:42 +0700

    update .gitignore

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ class LoginData(BaseModel):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
